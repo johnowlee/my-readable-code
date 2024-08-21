970f37d089f659bd2badd59bd74dfd4442fd72cd
johnowlee <johnowlee@gmail.com>
2024-08-21 18:04:34 +0900

    LSP 위반 해결

--- src/main/java/cleancode/minesweeper/tobe/GameBoard.java
+++ src/main/java/cleancode/minesweeper/tobe/GameBoard.java
@@ -1,6 +1,6 @@
 package cleancode.minesweeper.tobe;
 
-import cleancode.minesweeper.tobe.cell.Cell2;
+import cleancode.minesweeper.tobe.cell.Cell;
 import cleancode.minesweeper.tobe.cell.EmptyCell;
 import cleancode.minesweeper.tobe.cell.LandMineCell;
 import cleancode.minesweeper.tobe.cell.NumberCell;
@@ -10,25 +10,25 @@ import java.util.Arrays;
 import java.util.Random;
 
 public class GameBoard {
-    private final Cell2[][] board;
+    private final Cell[][] board;
 
     private final int landMineCount;
 
     public GameBoard(GameLevel gameLevel) {
         int rowSize = gameLevel.getRowSize();
         int colSize = gameLevel.getColSize();
-        board = new Cell2[rowSize][colSize];
+        board = new Cell[rowSize][colSize];
 
         landMineCount = gameLevel.getLandMineCount();
     }
 
     public void flag(int rowIndex, int colIndex) {
-        Cell2 cell = findCell(rowIndex, colIndex);
+        Cell cell = findCell(rowIndex, colIndex);
         cell.flag();
     }
 
     public void open(int rowIndex, int colIndex) {
-        Cell2 cell = findCell(rowIndex, colIndex);
+        Cell cell = findCell(rowIndex, colIndex);
         cell.open();
     }
 
@@ -72,9 +72,7 @@ public class GameBoard {
         for (int i = 0; i < landMineCount; i++) {
             int landMineCol = new Random().nextInt(colSize);
             int landMineRow = new Random().nextInt(rowSize);
-            LandMineCell landMineCell = new LandMineCell();
-            landMineCell.turnOnLandMine();
-            board[landMineRow][landMineCol] = landMineCell;
+            board[landMineRow][landMineCol] = new LandMineCell();;
         }
 
         for (int row = 0; row < rowSize; row++) {
@@ -86,9 +84,7 @@ public class GameBoard {
                 if (count == 0) {
                     continue;
                 }
-                NumberCell numberCell = new NumberCell();
-                numberCell.updateNearbyLandMineCount(count);
-                board[row][col] = numberCell;
+                board[row][col] = new NumberCell(count);
             }
         }
 
@@ -103,12 +99,12 @@ public class GameBoard {
     }
 
     public String getSign(int rowIndex, int colIndex) {
-        Cell2 cell = findCell(rowIndex, colIndex);
+        Cell cell = findCell(rowIndex, colIndex);
         return cell.getSign();
     }
 
     public boolean isLandMineCell(int selectedRowIndex, int selectedColIndex) {
-        Cell2 cell = findCell(selectedRowIndex, selectedColIndex);
+        Cell cell = findCell(selectedRowIndex, selectedColIndex);
         return cell.isLandMine();
     }
 
@@ -126,7 +122,7 @@ public class GameBoard {
         return findCell(row, col).isOpened();
     }
 
-    private Cell2 findCell(int rowIndex, int colIndex) {
+    private Cell findCell(int rowIndex, int colIndex) {
         return board[rowIndex][colIndex];
     }
 
--- src/main/java/cleancode/minesweeper/tobe/cell/LandMineCell.java
+++ src/main/java/cleancode/minesweeper/tobe/cell/LandMineCell.java
@@ -1,19 +1,8 @@
 package cleancode.minesweeper.tobe.cell;
 
-public class LandMineCell extends Cell2 {
+public class LandMineCell extends Cell {
 
-    private boolean isLandMine;
     private static final String LAND_MINE_SIGN = "☼";
-
-    @Override
-    public void turnOnLandMine() {
-        isLandMine = true;
-    }
-
-    @Override
-    public void updateNearbyLandMineCount(int count) {
-        throw new UnsupportedOperationException("지원하지 않는 기능입니다.");
-    }
 
     @Override
     public boolean isLandMine() {
